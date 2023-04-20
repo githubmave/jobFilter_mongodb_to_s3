3f7604e87048694e969b4f8566c5c5d06416b4ba
Mave li <lidanmei@yahoo.com>
2023-04-20 22:12:43 +1200

    sdaf

--- db_to_s3.py
+++ db_to_s3.py
@@ -31,3 +31,20 @@ jobcategories_json = jobcategories_filter(jobcategories_collection)
 try:
 
 	upload_s3(s3_client, jobcategories_json, os.environ['LOAD_BUCKET_NAME'], 'jobcategories.json')
+	print('users.json has been upload to s3')
+except Exception as error:
+	print(error)
+	
+# upload cities collection
+cities_collection = get_collection(mongodb_client, os.environ['MONGODB_DATABE'],'cities')
+cities_json =cities_filter(cities_collection)
+
+try:
+	upload_s3(s3_client,cities_json,os.environ['LOAD_BUCKET_NAME'],'cities.json')
+	print('cities.json has been upload to s3')
+except Exception as error:
+	 print(error)
+	 
+if __name__=='__main__':
+	run()
+	 
